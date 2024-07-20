de2b7232edf44b5bb7ed1e18b300787c988f2829
Antal P. <antal.perger@gmail.com>
2024-07-20 23:27:51 +0200

    test.http

--- srv/mysimplesrv.js
+++ srv/mysimplesrv.js
@@ -6,15 +6,38 @@ const { Students } = cds.entities("myCompany.hr.lms");
 
 module.exports = (srv) => {
 
-    srv.on("READ", "StudentSRV", (req, res) => {
+    srv.on("READ", "StudentSRV", async (req, res) => {
         console.log("hello");
 
         const { SELECT } = cds.ql;
-        const result = SELECT.from(Students);
-        console.log(result);
-        return result;
+        const aFilter = req.query.SELECT.from.ref[0].where;
+        console.log(aFilter);
+        //const result = await SELECT.from(Students).where({ email: "demo@demo.com" });
+        if (typeof aFilter === "undefined") {
+            return await SELECT.from(Students);
+        } else {
+            return await SELECT.from(Students).where({ email: aFilter[2].val });
+        }
+
 
     });
 };
+
+// req.query.SELECT.from.ref[0].where[2].val
+// 'demo@demo.com'
+
+// demoP = new Promise()
+// demoP
+//    .then((resolve, reject) => {
+//        // code when resolved
+//        result = value;
+//    })
+//    .catch(err => {
+//
+//    })
+//    .final(() => {
+//
+//    });
+
 
 
