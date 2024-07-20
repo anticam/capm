2af6d428b6c759ffae4ba99a25cdd6df1078648c
Antal P. <antal.perger@gmail.com>
2024-07-20 22:13:26 +0200

    debugging

--- srv/mysimplesrv.js
+++ srv/mysimplesrv.js
@@ -1,9 +1,18 @@
+const cds = require("@sap/cds");
+
+// namespace of the entities
+const { Students } = cds.entities("myCompany.hr.lms");
 
 
 module.exports = (srv) => {
 
-    srv.before("READ", "StudentSRV", (req, res) => {
+    srv.on("READ", "StudentSRV", (req, res) => {
         console.log("hello");
+
+        const { SELECT } = cds.ql;
+        const result = SELECT.from(Students);
+        console.log(result);
+        return result;
 
     });
 };
